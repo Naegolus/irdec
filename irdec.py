6c8e7692478b1fc8b60120cde8823c9e915a4bb3
Johannes Natter <office@dsp-crowd.com>
2017-06-12 12:33:04 +0200

    extract R401A decoder from irdec.py. Don't decode anything at default

--- irdec.py
+++ irdec.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sys, os
+import sys, os; sys.dont_write_bytecode = True
 import argparse
-
-parser = argparse.ArgumentParser(formatter_class = argparse.ArgumentDefaultsHelpFormatter, description = 'Panasonic R401A infrared communication decoder. Input: $ mode2 -d /dev/lirc0')
-#parser.add_argument('--hdr-mark', default = 3500, help = 'Header mark')
-#parser.add_argument('--bit-mark', default = 440, help = 'Bit mark')
-#parser.add_argument('--tail-mark', default = 440, help = 'Tail mark')
-parser.add_argument('--tail-space', default = 9950, help = 'Tail space')
-parser.add_argument('--hdr-space', default = 1700, help = 'Header space')
-parser.add_argument('--bit-one-space', default = 1300, help = 'Bit one space')
-parser.add_argument('--bit-zero-space', default = 440, help = 'Bit zero space')
-#parser.add_argument('-s', '--bit-swap', choices = [0, 1], default = 1, help = 'Set bit swapping true or false')
-args = parser.parse_args()
+import dec_R401A
 
 def createThresholds():
 	global tailToHdr, hdrToOne, oneToZero
@@ -42,124 +32,11 @@ def getTokenType(sigLen):
 	else:
 		return 4 # Zero
 
-constHeader = [2, 32, 224, 4, 0, 0, 0, 6]
-def checkHeaderOk(data):
-	global headerOk
-
-	#print data
-
-	if (len(data) < len(constHeader)):
-		#print 'len(data) < len(constHeader)'
-		return False
-
-	for i in range(0, len(constHeader)):
-		if (data[i] != constHeader[i]):
-			#print 'data[%d] != constHeader[%d]' % (i, i)
-			return False
-
-	return True
-
-def printData(data):
-	global headerOk
-
-	#print data
-
-	if (len(data) < 19):
-		print 'len(data) < 19, aborting'
-		return
-
-	if (headerOk):
-		print 'Header:\t\tOK'
-	else:
-		print 'Header:\t\tNot OK'
-		sys.stdout.write('  Should be:\t')
-		for i in range(0, len(constHeader)):
-			hexByte = '%0.2X' % constHeader[i]
-			sys.stdout.write(hexByte)
-			if (i == 2 or i == 5):
-				sys.stdout.write(' ')
-		print ''
-
-	checkSum = 0
-	for i in range(0, 17):
-		checkSum += data[i]
-	checkSum = checkSum & 255
-
-	if (checkSum == data[18]):
-		print 'Checksum:\tOK'
-	else:
-		print 'Checksum:\tDiffers'
-		print '  Calculated:\t%0.2X' % checkSum
-
-	switch = data[5] & 0b1
-	#hexData = '%0.2X' % data[5]
-	#print hexData
-	if (switch):
-		print 'Power:\t\tON'
-	else:
-		print 'Power:\t\tOFF'
-
-	mode = data[5] >> 4
-	if (mode == 0):
-		print 'Mode:\t\tAUTO'
-	elif (mode == 2):
-		print 'Mode:\t\tDRY'
-	elif (mode == 3):
-		print 'Mode:\t\tCOOL'
-	elif (mode == 4):
-		print 'Mode:\t\tHEAT'
-	elif (mode == 6):
-		print 'Mode:\t\tFAN'
-	else:
-		print 'Mode:\t\t<unknown>'
-
-	temp = data[6] >> 1 # = Byte 7
-	print 'Temperature:\t%d°C' % temp
-
-	#profile = data[13]
-	#if (profile == 16):
-		#print 'Profile: NORMAL'
-	#elif (profile == 17):
-		#print 'Profile: BOOST'
-	#elif (profile == 48):
-		#print 'Profile: QUIET'
-	#else:
-		#print 'Profile: <unknown>'
-
-	swing = data[8] & 0b1111
-	if (swing == 15):
-		print 'Swing:\t\tAUTO'
-	elif (swing == 1):
-		print 'Swing:\t\t1 (Horizontal)'
-	elif (swing == 2):
-		print 'Swing:\t\t2'
-	elif (swing == 3):
-		print 'Swing:\t\t3'
-	elif (swing == 4):
-		print 'Swing:\t\t4'
-	elif (swing == 5):
-		print 'Swing:\t\t5 (Vertical)'
-	else:
-		print 'Swing:\t\t<unknown>'
-
-	fanSpeed = data[8] >> 4
-	if (fanSpeed == 10):
-		print 'Fan Speed:\tAUTO'
-	elif (fanSpeed == 3):
-		print 'Fan Speed:\t1 (Slowest)'
-	elif (fanSpeed == 4):
-		print 'Fan Speed:\t2'
-	elif (fanSpeed == 5):
-		print 'Fan Speed:\t3'
-	elif (fanSpeed == 6):
-		print 'Fan Speed:\t4'
-	elif (fanSpeed == 7):
-		print 'Fan Speed:\t5 (Fastest)'
-	else:
-		print 'Fan Speed:\t<unknown>'
+def dummyDecodeFrame(frameId, frameData):
+	pass
 
 def processLine(line):
-	global strByte, cntBit, cntByte, data, headerOk
+	global strByte, cntBit, cntByte, frameId, frameData
 
 	if processLine.first:
 		processLine.first = False
@@ -172,13 +49,25 @@ def processLine(line):
 
 		if (tokenType == 0):
 			print ''
+
+			decodeFrame(frameId, frameData)
+
+			frameData = []
+			frameId = 0
+
+			strByte = ''
+			cntBit = 0
 			cntByte = 0
-			printData(data)
-			data = []
 		elif (tokenType == 1):
 			sys.stdout.write('  ')
-			headerOk = checkHeaderOk(data)
-			data = []
+
+			decodeFrame(frameId, frameData)
+
+			frameData = []
+			frameId += 1
+
+			strByte = ''
+			cntBit = 0
 			cntByte = 0
 		elif (tokenType == 3):
 			strByte += '1'
@@ -191,7 +80,7 @@ def processLine(line):
 
 		if (cntBit >= 8):
 			byte = int(strByte[::-1], 2)
-			data.append(byte)
+			frameData.append(byte)
 			hexByte = '%0.2X' % byte
 			sys.stdout.write(hexByte)
 			#sys.stdout.write('_')
@@ -199,19 +88,37 @@ def processLine(line):
 			cntBit = 0
 			cntByte += 1
 
-			if (cntByte % 3 == 0):
+			if (cntByte % 4 == 0):
 				sys.stdout.write(' ')
 
 processLine.first = True
 
+# Parse Arguments
+parser = argparse.ArgumentParser(formatter_class = argparse.ArgumentDefaultsHelpFormatter, description = 'Infrared communication decoder. Input: $ mode2 -d /dev/lirc0')
+#parser.add_argument('--hdr-mark', default = 3500, help = 'Header mark')
+#parser.add_argument('--bit-mark', default = 440, help = 'Bit mark')
+#parser.add_argument('--tail-mark', default = 440, help = 'Tail mark')
+parser.add_argument('-t', '--tail-space', default = 9950, help = 'Tail space')
+parser.add_argument('-r', '--hdr-space', default = 1700, help = 'Header space')
+parser.add_argument('-o', '--bit-one-space', default = 1300, help = 'Bit one space')
+parser.add_argument('-z', '--bit-zero-space', default = 440, help = 'Bit zero space')
+#parser.add_argument('-s', '--bit-swap', choices = [0, 1], default = 1, help = 'Set bit swapping true or false')
+parser.add_argument('-d', '--decoder', choices = ['R401A'], help = 'Set specific decoder. R401A .. Panasonic R401A inverter')
+args = parser.parse_args()
+
+if (args.decoder == 'R401A'):
+	decodeFrame = dec_R401A.decodeR401aFrame
+else:
+	decodeFrame = dummyDecodeFrame
+
 # Init
 createThresholds()
 k = 0
+frameData = []
+frameId = 0
 strByte = ''
-data = []
 cntBit = 0
 cntByte = 0
-headerOk = False
 
 # Processing
 try:
